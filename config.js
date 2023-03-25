b6ed1c2eba8a5b4ffc6fb39a1ad5cdf52c4f3c15
xanmann <128101423+xanmann@users.noreply.github.com>
2023-03-25 17:37:50 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "", // place your token here (no token = QR Login)
+    password: "", // The password is mandatory to change the nickname & tag
+    username: "xvids", // The username to snipe (no username === no work)
+    tag: "0001" // The tag to snipe (no tag === no work)
+}
